63695e509b06e33c007a426f1f5bb0b8d8aa02cf
Ankan2810 <22130111@st.hcmuaf.edu.vn>
2024-03-06 12:43:31 +0700

    update

--- script.js
+++ script.js
@@ -1,17 +1,48 @@
-function makeSnow(){
-    const snow = document.createElement('div');
-    const size = Math.random() *4.5 + 3.5;
-    snow.className = 'snow';
-    snow.style.width = size + 'px';
-    snow.style.height = size + 'px';
-    snow.style.left = Math.random() * window.innerWidth + 'px';
-    snow.style.opacity = size/8;
-    if(size <7){
-        snow.style.zIndex= -5;
-    }
-    snow.style.animationDuration = Math.random() * 3 + 2 + 's';
-    document.body.appendChild(snow);
-    setTimeout(() => snow.remove(), 5000);
-}
+$(document).ready(function() {
 
-setInterval(makeSnow, 40);
+    var envelope = $('#envelope');
+    var btn_open = $("#open");
+    var btn_reset = $("#reset");
+    var letter = $(".letter"); // Select the letter element
+    var hidden = $("#hiddenLetter");  // Create an empty jQuery object to store
+    // Open envelope on clicking the letter or open button
+    letter.click(function() {
+        openLetter();
+    });
+    btn_open.click(function() {
+        open();
+    });
+    btn_reset.click(function() {
+        close();
+    });
+
+    function open() {
+        envelope.addClass("open")
+            .removeClass("close");
+    }
+
+    function close() {
+        envelope.addClass("close")
+            .removeClass("open")
+            .removeClass("hidden");
+        hidden.addClass("hidden")
+                .removeClass("visible");
+    }
+
+    function openLetter(){
+        hidden.addClass("visible")
+        .removeClass("hidden");
+        envelope.addClass("hidden")
+        .removeClass("open");
+    }
+
+});
+
+function onDevToolsOpen() {
+    console.log("Code không clean xin đừng xem :<"); // Log a message
+    // Remove unnecessary setTimeout calls
+  }
+  
+  // Call the function to log a message when DevTools opens (optional)
+  onDevToolsOpen();
+  
